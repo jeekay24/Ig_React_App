a4f1d64047ef95f0b14d4348a1e7fa7a587b1c66
Ganesh Kumar <rjeekay24@gmail.com>
2024-10-24 00:20:30 +0530

    Configured Tailwind CSS

--- app/(tabs)/index.tsx
+++
@@ -1,8 +0,0 @@
-import { Text } from "react-native";
-
-export default function FeedScreen() {
-    return (
-      <Text> Feed Screen</Text>
-    );
-  }
-  
---
+++ src/app/(tabs)/index.tsx
@@ -0,0 +1,10 @@
+import { Text, View} from "react-native";
+
+export default function FeedScreen() {
+    return (
+      <View className='bg-slate-600 flex-1 items-center justify-center '>
+      <Text className='text-3xl font-bold text-blue-500'> Feed </Text>
+      </View>
+    );
+  }
+  
