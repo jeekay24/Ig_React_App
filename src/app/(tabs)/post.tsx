1e9e182112ba0f8d6f9d79284829b366df261c47
Ganesh Kumar <rjeekay24@gmail.com>
2024-10-25 23:44:42 +0530

    Added image picker

--- src/app/(tabs)/post.tsx
+++ src/app/(tabs)/post.tsx
@@ -1,16 +1,42 @@
 import { Text, View, Image, TextInput, Pressable } from "react-native";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import * as ImagePicker from 'expo-image-picker';
 
 export default function CreatePost() {
   const [caption, setCaption] = useState('');
+  const [image, setImage] = useState<string | null>(null);
+
+  useEffect(() => {
+    if(!image){
+      pickImage();
+    }
+  }, [image]);
+
+  const pickImage = async () => {
+    // No permissions request is necessary for launching the image library
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      setImage(result.assets[0].uri);
+    }
+  };
+
     return (
       <View className="p-3 items-center flex-1">
         { /* Image Picker */ }
-        <Image source={{uri: "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/2.jpg"}}
+        {image ? (<Image source={{uri: image}}
         className="w-64 aspect-[3/4] rounded-lg shadow-md"
         />
+        ) : (
+          <View className="w-64 aspect-[3/4] rounded-lg shadow-md" />
+        )}
 
-        <Text onPress={() => {}} className="text-blue-500 font-semibold m-5"> Change</Text>
+        <Text onPress={pickImage} className="text-blue-500 font-semibold m-5"> Change</Text>
         
         { /* TextInput for caption */ }
         <TextInput value={caption} onChangeText={(newValue) => setCaption(newValue)} placeholder="What's on your mind" className=" w-full p-3" />
