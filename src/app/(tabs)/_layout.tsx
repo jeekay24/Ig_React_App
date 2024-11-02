48dc7c04db01f73d2ed1533ccd7b68a7129db0e0
Ganesh Kumar <rjeekay24@gmail.com>
2024-11-02 21:02:08 +0530

    Changed feed name to instagram

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -5,7 +5,7 @@ export default function TabsLayout(){
     return (
         <Tabs screenOptions={{tabBarActiveTintColor:"black",tabBarShowLabel:false}}>
 
-            <Tabs.Screen name="index" options={{headerTitle: "Feed",headerTitleAlign:"center",tabBarIcon: ({color}) => <FontAwesome name="home" size={26} color={color} />}}/>
+            <Tabs.Screen name="index" options={{headerTitle: "Instagram",headerTitleAlign:"center",tabBarIcon: ({color}) => <FontAwesome name="home" size={26} color={color} />}}/>
 
             <Tabs.Screen name="post" options={{headerTitle: "Create Post",headerTitleAlign:"center",tabBarIcon: ({color}) => <FontAwesome name="plus-square-o" size={26} color={color} />}}/>
 
